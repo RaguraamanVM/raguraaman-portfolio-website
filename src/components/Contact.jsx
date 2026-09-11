26baff29e525d51abfaa18c3afb56c899944ca79
Raguraaman <143984449+RaguraamanVM@users.noreply.github.com>
2026-09-11 13:56:06 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ export default function Contact() {
           <Linkedin size={18} strokeWidth={1.6} />
           <div>
             <span className="endpoint__label">linkedin</span>
-            <span className="endpoint__value">raguraaman-v-m</span>
+            <span className="endpoint__value">raguraaman/</span>
           </div>
         </a>
       </div>
